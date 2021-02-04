74776b458c474fe33c3410a4c96004443b2ad915
rendytf <rendytf26@gmail.com>
2021-02-04 22:25:45 +0700

    organize file and folder

---
+++ src/supports/Notes.js
@@ -0,0 +1,4 @@
+// Reactstrap VS Bootstrap
+    // Reactstrap ==> component siap pakai
+    // Bootstrap ==> class siap pakai
+    // Menggunakan reactsrap ?? karena JQuery di Bootstrap tidak di rekomendasikan di React
